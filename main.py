026338d04ccf8c0a8e469845f5eb01030513f476
Edgar Daniel <danichg94@gmail.com>
2018-11-01 00:15:24 -0600

    Update main.py

--- main.py
+++ main.py
@@ -9,69 +9,85 @@ if sys.version_info[0] < 3:
 else:
     import tkinter as Tk
 
-class Plot():
-    def __init__(self, root, data1, data2, nombres, ind, width):
+
+class Canvas():
+    def __init__(self, root, data, nombres, ind, width):
         self.root = root
-        self.data1 = data1
-        self.data2 = data2
+        self.data = data        
         self.nombres = nombres
         self.ind = ind
         self.width = width
-        self.canvas1, self.canvas2 = self.canvas()
-
-    def menu(self):
-        page1btn = Tk.Button(self.root, text="Page 1", command=self.page1)
-        page2btn = Tk.Button(self.root, text="Page 2", command=self.page2)
-        page1btn.pack()
-        page2btn.pack()
-        
-    def page1(self):
-        #canvas1, canvas2 = self.canvas()
-        self.canvas2.get_tk_widget().pack_forget()
-        self.canvas1.get_tk_widget().pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
-
-    def page2(self):
-        #canvas1, canvas2 = self.canvas()
-        self.canvas1.get_tk_widget().pack_forget()
-        self.canvas2.get_tk_widget().pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
+        self.canvas = self.canvas()
 
     def canvas(self):
-        ##################################################
-        f1 = Figure(figsize=(5,4), dpi=100)
-        f2 = Figure(figsize=(5,4), dpi=100)
-        ax1 = f1.add_subplot(111)
-        ax2 = f2.add_subplot(111)
+        f = Figure(figsize=(5,4), dpi=100)     
+        ax = f.add_subplot(111)     
+        ax.set_xlabel("estudiantes")        
+        ax.set_ylabel("notas")
+        ax.set_xticks(self.ind+self.width/2)
+        ax.set_xticklabels(self.nombres) #
+        rects = ax.bar(self.ind, self.data, self.width)
+        canvas = FigureCanvasTkAgg(f, master=self.root)
+        canvas.show()
+        return canvas
 
-        ax1.set_xlabel("estudiantes")
-        ax2.set_xlabel("estudiantes")
-        ax1.set_ylabel("notas")
-        ax2.set_ylabel("notas")
-        ax1.set_xticks(self.ind+self.width/2)
-        ax2.set_xticks(self.ind+self.width/2)
-        ax1.set_xticklabels(self.nombres) #
-        ax2.set_xticklabels(self.nombres) #
+#class Pages():
+#    def __init__(self, canvas):
+#        self.canvas = canvas
+        
+#    def menu(self):
+#        page1btn = Tk.Button(self.root, text="Page 1", command=self.page1)
+#        page2btn = Tk.Button(self.root, text="Page 2", command=self.page2)
+#        page1btn.pack()
+#        page2btn.pack()
 
-        rects1 = ax1.bar(self.ind, self.data1, self.width)
-        rects2 = ax2.bar(self.ind, self.data2, self.width)
-        canvas1 = FigureCanvasTkAgg(f1, master=root)
-        canvas1.show()
-        canvas2 = FigureCanvasTkAgg(f2, master=root)
-        canvas2.show()
-        ##################################################
-        return canvas1, canvas2
+#    def page1(self):
+        #canvas1, canvas2 = self.canvas()
+#        self.canvas2.get_tk_widget().pack_forget()
+#        self.canvas1.get_tk_widget().pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
 
-data1 = (90, 85, 95)       #
-data2 = (85, 95, 100)       #
+#    def page2(self):
+        #canvas1, canvas2 = self.canvas()
+#        self.canvas1.get_tk_widget().pack_forget()
+#        self.canvas2.get_tk_widget().pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
+
+class Pages():
+    def __init__(self, canvas):
+        self.canvas = canvas
+        self.page0btn = Tk.Button(root, text="Page 0", command=self.page0)
+        self.page1btn = Tk.Button(root, text="Page 1", command=self.page1)
+        
+    def page0(self):
+        self.canvas.get_tk_widget().pack_forget()
+        self.page0btn.pack_forget()
+
+        self.page1btn.pack()
+
+    def page1(self):        
+        self.page1btn.pack_forget()
+        
+        self.canvas.get_tk_widget().pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
+        self.page0btn.pack()
+        
+
+
+data = (90, 85, 95)       #
 nombres = ['a', 'b', 'c']
-ind = numpy.arange(len(data1))  # the x locations for the groups
+ind = numpy.arange(len(data))  # the x locations for the groups
 width = .5
-#canvas1, canvas2 = canvas(data1, data2, nombres, ind, width)
 
 
 root = Tk.Tk()
 
-plot = Plot(root, data1, data2, nombres, ind, width)
+canvas1 = Canvas(root, data, nombres, ind, width)
+canvas2 = Canvas(root, data, nombres, ind, width)
 
-plot.menu()
+pages = Pages(canvas1.canvas)
+
+pages.page0()
+
+
+#canvas1.menu()
+#canvas2.menu()
 
 Tk.mainloop()
