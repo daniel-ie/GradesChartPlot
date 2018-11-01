431806b1d87d5b7cc265cdaf6d6c1784d8885b29
Edgar Daniel <danichg94@gmail.com>
2018-11-01 11:26:18 -0600

    Update main.py

--- main.py
+++ main.py
@@ -22,67 +22,98 @@ class Canvas():
 
     def canvas(self):
         f = Figure(figsize=(5,4), dpi=100)     
-        ax = f.add_subplot(111)        
+        ax = f.add_subplot(111)
+        
         ax.set_xlabel("estudiantes")        
         ax.set_ylabel("notas")
         ax.set_xticks(self.ind+self.width/2)
         ax.set_xticklabels(self.nombres) #
         rects = ax.bar(self.ind, self.data, self.width)
-        canvas = FigureCanvasTkAgg(f, master=self.root)
+        canvas = FigureCanvasTkAgg(f, master=root)#self.root)
         #canvas.get_tk_widget().pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
         canvas.show()
         return canvas
 
+    #def canvasEmpty(self):
+        #f = Figure(figsize=(5,4), dpi=100)     
+        #ax = f.add_subplot(111)
+        
+        #ax.set_xlabel("estudiantes")        
+        #ax.set_ylabel("notas")
+        #ax.set_xticks(self.ind+self.width/2)
+        #ax.set_xticklabels(self.nombres) #
+        #rects = ax.bar(self.ind, self.data, self.width)
+        #canvas = FigureCanvasTkAgg(f, master=root)#self.root)
+        ##canvas.get_tk_widget().pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
+        #canvas.show()
+        #return canvas
 
 class Pages():
-    def __init__(self, canvasS, canvasE, canvasS_clase, canvasE_clase):#, canvasS, canvasE):
+    def __init__(self, canvasS, canvasE, canvasS_clase, canvasE_clase):
         self.canvasS = canvasS
         self.canvasE = canvasE
         self.canvasS_clase = canvasS_clase
         self.canvasE_clase = canvasE_clase
-        self.page0btn = Tk.Button(root, text="Inicio", command=self.page0)
-        self.page1btn = Tk.Button(root, text="Notas por estudiante", command=self.page1)
-        self.page1btn_clase = Tk.Button(root, text="Notas por clase", command=self.page1_clase)
+        #self.canvasEmpty = canvasEmpty
 
-        self.page2btnE = Tk.Button(root, text="Español", command=self.pageE)
-        self.page2btnS = Tk.Button(root, text="Sociales", command=self.pageS)
+        self.back = back
+        self.page0btn = Tk.Button(master=self.back, text="Inicio", command=self.page0)
+        self.page1btn = Tk.Button(master=self.back, text="Notas por estudiante", command=self.page1)
+        self.page1btn_clase = Tk.Button(master=self.back, text="Notas por clase", command=self.page1_clase)
 
-        self.page2btnE_clase = Tk.Button(root, text="Español", command=self.pageE_clase)
-        self.page2btnS_clase = Tk.Button(root, text="Sociales", command=self.pageS_clase)
+        #self.nxe = Tk.Title(master=self.back, text="Notas por estudiante")
+
+        self.page2btnE = Tk.Button(master=self.back, text="Español", command=self.pageE)
+        self.page2btnS = Tk.Button(master=self.back, text="Sociales", command=self.pageS)
+
+        self.page2btnE_clase = Tk.Button(master=self.back, text="Español", command=self.pageE_clase)
+        self.page2btnS_clase = Tk.Button(master=self.back, text="Sociales", command=self.pageS_clase)
 
     def page0(self):           # inicio
         self.canvasS.get_tk_widget().pack_forget()
         self.canvasE.get_tk_widget().pack_forget()
+        self.canvasS_clase.get_tk_widget().pack_forget()
+        self.canvasE_clase.get_tk_widget().pack_forget()
         self.page0btn.pack_forget()
         self.page2btnE.pack_forget()
-        self.page2btnS.pack_forget()        
+        self.page2btnS.pack_forget()
+        self.page2btnE_clase.pack_forget()
+        self.page2btnS_clase.pack_forget()          
 
-        self.page1btn.pack()
-        self.page1btn_clase.pack()
+        self.back.pack()
+        root.title("Graficar notas")
+        self.page1btn.pack(fill=Tk.BOTH, expand=1)
+        self.page1btn_clase.pack(fill=Tk.BOTH, expand=1)
 
-    def page1(self):        
-        self.canvasS.get_tk_widget().pack_forget()
-        self.canvasE.get_tk_widget().pack_forget()
-        self.canvasS_clase.get_tk_widget().pack_forget()
-        self.canvasE_clase.get_tk_widget().pack_forget()        
-        #self.page0btn.pack_forget()
-        self.page1btn_clase.pack_forget()
-
-        self.page0btn.pack()   # hacia pagina de inicio
-        self.page2btnE.pack()  # plot de notas de español 
-        self.page2btnS.pack()  # plot de notas de sociales 
-
-    def page1_clase(self):        
+    def page1(self):          # notas por estudiante 
         self.canvasS.get_tk_widget().pack_forget()
         self.canvasE.get_tk_widget().pack_forget()
         self.canvasS_clase.get_tk_widget().pack_forget()
         self.canvasE_clase.get_tk_widget().pack_forget()        
         #self.page0btn.pack_forget()
         self.page1btn.pack_forget()
+        self.page1btn_clase.pack_forget()
 
-        self.page0btn.pack()   # hacia pagina de inicio
-        self.page2btnE_clase.pack()  # plot de notas de español 
-        self.page2btnS_clase.pack()  # plot de notas de sociales 
+        self.back.pack()
+        root.title("Notas por estudiante")
+        self.page0btn.pack(side=Tk.LEFT, fill=Tk.BOTH, expand=1)   # hacia pagina de inicio
+        self.page2btnE.pack(fill=Tk.BOTH, expand=1)  # plot boton de notas de español 
+        self.page2btnS.pack(fill=Tk.BOTH, expand=1)  # plot boton de notas de sociales 
+
+    def page1_clase(self):    # notas por clase        
+        self.canvasS.get_tk_widget().pack_forget()
+        self.canvasE.get_tk_widget().pack_forget()
+        self.canvasS_clase.get_tk_widget().pack_forget()
+        self.canvasE_clase.get_tk_widget().pack_forget()        
+        #self.page0btn.pack_forget()        
+        self.page1btn.pack_forget()
+        self.page1btn_clase.pack_forget()
+
+        self.back.pack()
+        root.title("Notas por clase")
+        self.page0btn.pack(side=Tk.LEFT, fill=Tk.BOTH, expand=1)   # hacia pagina de inicio
+        self.page2btnE_clase.pack(fill=Tk.BOTH, expand=1)  # plot de notas de español 
+        self.page2btnS_clase.pack(fill=Tk.BOTH, expand=1)  # plot de notas de sociales 
 
         
     def pageE(self):        
@@ -90,34 +121,43 @@ class Pages():
         self.canvasS.get_tk_widget().pack_forget()
         self.canvasS_clase.get_tk_widget().pack_forget()
         self.canvasE_clase.get_tk_widget().pack_forget()        
-        
-        self.canvasE.get_tk_widget().pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)       
-        self.page0btn.pack()   # hacia pagina de inicio
+
+        self.back.pack()
+        root.title("Notas de español promediadas por estudiante")
+        self.canvasE.get_tk_widget().pack(fill=Tk.BOTH, expand=1)       
+        self.page0btn.pack(side=Tk.LEFT, fill=Tk.BOTH, expand=1)   # hacia pagina de inicio
     def pageS(self):        
         self.page1btn.pack_forget()
         self.canvasE.get_tk_widget().pack_forget()
         self.canvasS_clase.get_tk_widget().pack_forget()
         self.canvasE_clase.get_tk_widget().pack_forget()        
-        
-        self.canvasS.get_tk_widget().pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)       
-        self.page0btn.pack()   # hacia pagina de inicio
+
+        self.back.pack()
+        root.title("Notas de Sociales promediadas por estudiante")
+        self.canvasS.get_tk_widget().pack(fill=Tk.BOTH, expand=1)       
+        self.page0btn.pack(side=Tk.LEFT, fill=Tk.BOTH, expand=1)   # hacia pagina de inicio
 
     def pageE_clase(self):
-        self.page1btn.pack_forget()
+        self.page1btn_clase.pack_forget()
         self.canvasS.get_tk_widget().pack_forget()
         self.canvasE.get_tk_widget().pack_forget()        
         self.canvasS_clase.get_tk_widget().pack_forget()
+
+        self.back.pack()
+        root.title("Notas de español promediadas por clase")
+        self.canvasE_clase.get_tk_widget().pack(fill=Tk.BOTH, expand=1)       
+        self.page0btn.pack(side=Tk.LEFT, fill=Tk.BOTH, expand=1)   # hacia pagina de inicio
         
-        self.canvasE_clase.get_tk_widget().pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)       
-        self.page0btn.pack()   # hacia pagina de inicio
     def pageS_clase(self):        
-        self.page1btn.pack_forget()
+        self.page1btn_clase.pack_forget()
         self.canvasS.get_tk_widget().pack_forget()
         self.canvasE.get_tk_widget().pack_forget()        
         self.canvasE_clase.get_tk_widget().pack_forget()
 
-        self.canvasS_clase.get_tk_widget().pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)       
-        self.page0btn.pack()   # hacia pagina de inicio
+        self.back.pack()
+        root.title("Notas de Sociales promediadas por clase")
+        self.canvasS_clase.get_tk_widget().pack(fill=Tk.BOTH, expand=1)       
+        self.page0btn.pack(side=Tk.LEFT, fill=Tk.BOTH, expand=1)   # hacia pagina de inicio
 
 
 data = (90, 85, 95)       #
@@ -131,13 +171,17 @@ width = .5
 
 
 root = Tk.Tk()
+back = Tk.Frame(master=root, width=500, height=500, bg='black')
 #Text(root,width=60,height=40)
+
 
 canvasS = Canvas(root, data, nombres, ind, width)
 canvasE = Canvas(root, data2, nombres, ind, width)
 
 canvasS_clase = Canvas(root, data3, nombres, ind, width)
 canvasE_clase = Canvas(root, data4, nombres, ind, width)
+
+canvasEmpty = Canvas(root, data4, nombres, ind, width)
 
 pages = Pages(canvasS.canvas, canvasE.canvas, canvasS_clase.canvas, canvasE_clase.canvas)
 
@@ -148,3 +192,4 @@ pages.page0()
 #canvas2.menu()
 
 Tk.mainloop()
+
